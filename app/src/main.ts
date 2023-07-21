5c912e4cf1aa5b871ea78c9a956faabdf00fa7ea
Magno Machado <magnomp@gmail.com>
2023-07-21 20:00:33 -0300

    Unused

--- app/src/main.ts
+++ app/src/main.ts
@@ -8,7 +8,6 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import { useAuthStore } from "./stores/auth";
 import { useAuthTokenStore } from "./stores/authToken";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
 
